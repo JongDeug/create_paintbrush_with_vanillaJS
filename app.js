03374cef0eae1b809ddda7fc4044d262c4272836
JongDeug <pinpoint1998@gmail.com>
2022-04-13 17:28:56 +0900

    second-commit

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const canvas = document.querySelector("#jsCanvas");
 const ctx = canvas.getContext("2d");
 
+const DEFAULT_COLOR = "#2c2c2c";
 
+// set canvas background
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+// set canvas line color, line width
+ctx.fillStyle = DEFAULT_COLOR;
+ctx.strokeStyle = DEFAULT_COLOR;
+ctx.lineWidth = 2.5;
 
-
+// set booleans
+let filling = false;
 let painting = false;
+
 function stopPainting(){
     painting = false;
 }    
@@ -16,10 +26,8 @@ function startPainting(){
 function onMouseMove(e){
     const x = e.offsetX;
     const y = e.offsetY;
-    setColor();
-    setLineWidth();
 
-    // if mouse move, up, leave
+    // if mouse move, up
     if(!painting){
         ctx.beginPath();
         ctx.moveTo(x, y);
@@ -31,21 +39,22 @@ function onMouseMove(e){
     }
 }
 
-// function onMouseDown(e){
-//     painting = true;
-// }
-
-function onMouseLeave(e){
-    stopPainting();
+function handleCanvasClick(){
+    if(filling){
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
 }
-
 
 // set color
 function setColor(){
+    // return NodeList 
     const jsColors = document.querySelectorAll("#jsColors .controls_color");
+    
+    // NodeList have forEach function, but it's not Array 
     jsColors.forEach(element => {
         element.addEventListener("click", function(event){
             ctx.strokeStyle = event.target.style.backgroundColor;
+            ctx.fillStyle = event.target.style.backgroundColor;
         });
     });
 }
@@ -53,13 +62,64 @@ function setColor(){
 // set line width
 function setLineWidth(){
     const jsRange = document.querySelector("#jsRange");
-    ctx.lineWidth = jsRange.value;
+
+    if(jsRange){
+        // fires when the value of an element has been changed.
+        jsRange.addEventListener("input", function(){
+            console.log(jsRange.value);
+            ctx.lineWidth = jsRange.value;
+        })
+    }
+    else{
+        console.error("jsRange doesn't exist");
+    }
 }
 
+function setMode(){
+    const mode = document.querySelector("#jsMode");
+    if(mode){
+        mode.addEventListener("click", function(){
+            if(filling === true){
+                filling = false;
+                mode.innerText = "fill";
+            }else{
+                filling = true;
+                mode.innerText = "paint"   
+            }
+        });
+    }
+}
+
+function save(){
+    const saveBtn = document.querySelector("#jsSave");
+    if(saveBtn){
+        saveBtn.addEventListener("click", function(){
+
+            // 복습 하세용
+            const image = canvas.toDataURL();
+            const link = document.createElement("a");
+            link.href = image; //url 적구
+            link.download = "image/png"; //download하고 싶은 이미지 경로 적고 
+            console.log(link);
+            link.click();
+        })
+    }
+}
+
+function handleCM(e){
+    //우클릭 눌렀는데 막혔네, 기본 동작을 막음
+    e.preventDefault();
+}
 
 if(canvas){
     canvas.addEventListener("mousemove",onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM); // 마우스 우클릭
 }
+setColor();
+setLineWidth();
+setMode();
+save();
